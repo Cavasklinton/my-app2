6c727a450ece39113ce4da7ea66f89ceecf40fed
cavasklinton@plmun.edu.ph <cavasklinton@plmun.edu.ph>
2026-09-22 19:26:34 +0800

    Activity 5: Student registration form

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController;
+use App\Http\Controllers\RegistrationController;
 
 Route::get('/', [PageController::class, 'index']);
 Route::get('/about', [PageController::class, 'about']);
@@ -11,3 +12,5 @@ Route::view('/exercise-1', 'exercise1');
 Route::view('/exercise-2', 'exercise2');
 Route::view('/exercise-3', 'exercise3');
 Route::view('/exercise-4', 'exercise4');
+
+Route::get('/register', [RegistrationController::class, 'showForm']);
